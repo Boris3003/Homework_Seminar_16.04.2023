ae6522d8d5ee37053b75e54d81d8a592955f9647
Boris <D:\The Bat!>
2023-04-22 23:04:22 +0300

    ex003

---
+++ ex003/Program.cs
@@ -0,0 +1,10 @@
+﻿// Вывод заданного массива на экран
+Random random = new Random();
+int[] array = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    array[i] = random.Next(0, 100);
+}
+
+for (int i = 0; i < 8; i++)
+    Console.Write($"{array[i]} ; ");
