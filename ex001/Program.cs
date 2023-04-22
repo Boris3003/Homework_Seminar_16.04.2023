2c913d3172a082229f1a9a9f14b469a7906c3118
Boris <D:\The Bat!>
2023-04-22 22:43:19 +0300

    ex001

---
+++ ex001/Program.cs
@@ -0,0 +1,17 @@
+﻿// Возведение числа в степень
+int EnterNumber(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+int A = EnterNumber("Введите число: ");
+int B = EnterNumber("Введите степень числа: ");
+int x = 1;
+int y = 1;
+while (x <= B)
+{
+    y=y*A;
+    x++;
+}
+Console.WriteLine($"Число в степени равно: {y}");
