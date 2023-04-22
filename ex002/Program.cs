a62b80261806ad628c97d7a461f2d333687ab390
Boris <D:\The Bat!>
2023-04-22 22:51:38 +0300

    ex002

---
+++ ex002/Program.cs
@@ -0,0 +1,14 @@
+﻿// Сумма цифр в числе
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int x = 0;
+int y = 0;
+while (N > 0)
+{
+    x = N%10;
+    N = N/10;
+    y = y+x;
+}
+
+Console.WriteLine($"Сумма цифр в числе равна: {y}");
